90583e984886dc41b68b4b4f91a5ff0546c360ae
lucasbernardol <jose.lukass03@gmail.com>
2022-02-15 17:15:52 -0300

    chore: Create tag migrations

---
+++ src/core/database/migrations/1644955697151-CreateTags.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateTags1644955697151 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const tags = new Table({
+      name: 'tags',
+      columns: [
+        {
+          name: 'id',
+          type: 'uuid',
+          isPrimary: true,
+          isNullable: false,
+        },
+
+        {
+          name: 'name',
+          type: 'varchar',
+          length: '120',
+          isNullable: false,
+        },
+
+        {
+          name: 'description',
+          type: 'varchar',
+          length: '255',
+          isNullable: false,
+        },
+
+        {
+          name: 'created_at',
+          type: 'timestamp',
+          default: 'now()',
+        },
+
+        {
+          name: 'updated_at',
+          type: 'timestamp',
+          default: 'now()',
+        },
+      ],
+    });
+
+    await queryRunner.createTable(tags);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('tags');
+  }
+}
